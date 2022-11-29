0ac85b0a1abbbfe102411e095f40e9b43768b547
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-29 16:20:40 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+# Author - Oluwatosin
+
 def remove_char_at(str, n):
-    if n >= 0:
-        s1 = str[:n]
-        s2 = str[n + 1:]
-        return (s1 + s2)
-    else:
-        return (str)                                      return (str)
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
