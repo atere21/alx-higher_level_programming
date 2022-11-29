ced8791ab1dc99c81372a1f5e14a68d9495a9b6d
atere21 <tosinateret@gmail.com>
2022-11-29 06:14:23 -0800

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+scse(str):
+        for i in range(len(str)):
+                    j = ord(str[i])
+                            if (j >= 97) and (j <= 122):
+                                            j -= 32
+                                                    print("{}".format(chr(j)), end="")
+                                                        print()
