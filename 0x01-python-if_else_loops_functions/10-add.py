860503f7bd739b656275eec0450185cb6cbfa6f8
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-29 15:31:11 +0100

    Update 10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
+# Author - Oluwatosin
 def add(a, b):
-        """Return the addition of a and b."""
-            return (a + b)
+    """Return the addition of a and b."""
+    return (a + b)
