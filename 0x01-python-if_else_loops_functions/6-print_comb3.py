d2b5d28a3e7db1fee6b0f9e5e4a796c03dc19d89
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-29 13:59:02 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
-
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
-        else:
-            print("{}{}".format(digit1, digit2), end=", ")
+for i in range(0, 9):
+    for j in range(1, 10):
+        if j > i:
+            if i != 8:
+                print("{}{}, ".format(i, j), end="")
+            else:
+                print("{}{}".format(i, j))
