d9e2d559eae1e3dbba10ce281bc2e17cbf92054f
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-07 10:36:49 -0600

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-
-def weight_average(my_list=[]):
-
-        if my_list and len(my_list):
-
-                    num = 0
-
-                            denom = 0
-
-                                    for tup in my_list:
-
-                                                    num += (tup[0] * tup[1])
-
-                                                                denom += (tup[1])
-
-                                                                        return (num/denom)
-
-                                                                        return 0
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
