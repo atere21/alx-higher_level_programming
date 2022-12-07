c7692a320b909200f7635875618e895b1328675f
atere21 <tosinateret@gmail.com>
2022-12-07 08:16:47 -0800

    task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+ght_average(my_list=[]):
+
+        if my_list and len(my_list):
+
+                    num = 0
+
+                            denom = 0
+
+                                    for tup in my_list:
+
+                                                    num += (tup[0] * tup[1])
+
+                                                                denom += (tup[1])
+
+                                                                        return (num/denom)
+
+                                                                        return 0
