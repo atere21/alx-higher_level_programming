6244d96416aba577bdf22344f49ffa5e8e75cc66
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-19 11:39:00 +0100

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,10 @@
 import sys
 
 
-def safe_function(fct, *args):
+def safe_print_integer_err(value):
     try:
-        return fct(*args)
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
