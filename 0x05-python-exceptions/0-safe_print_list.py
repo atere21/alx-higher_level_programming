1e15b9c05c2f748cef3534b0f46be071499a3068
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-19 12:20:34 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-        total = 0
-            for i in range(x):
-                        try:
-                                        print(f"{my_list[i]}", end="")
-                                                    total += 1
-                                                            except IndexError:
-                                                                            break
+    total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total += 1
+        except IndexError:
+            break
+    print()
+    return(total)                  break
                                                                             print()
                                                                                 return(total)
