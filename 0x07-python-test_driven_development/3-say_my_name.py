f6abe1abc416f303ae1f94eddb2d0dfe96cad4c3
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-01-05 20:05:17 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
 """
-Function that prints My name is <first name> <last name>
+This module is composed by a function prints a message
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """ Function that prints "My name is <first name> <last name>"
+    Args:
+        first_name: first name
+        last_name: last name
+    Returns:
+        No return
+    Raises:
+        TypeError: If first_name or last_name is not a string
     """
-    Prints a Name
-    Arguments:
-    first_name: must be a string
-    last_name: must be a string, if no argument given, then empty by default
-    Otherwise a TypeError is raised
-    """
-    if not isinstance(first_name, str):
-        msg = "first_name must be a string"
-        raise TypeError(msg)
 
-    if not isinstance(last_name, str):
-        msg = "last_name must be a string"
-        raise TypeError(msg)
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))                                                                                        if len(set(checkl)) != 1:
-                                                                                                                            msg = "Each row of the matrix must have the same size"
-                                                                                                                                    raise TypeError(msg)
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
 
-                                                                                                                                    if not isinstance(div, (int, float)) or div != div:
-                                                                                                                                                msg = "div must be a number"
-                                                                                                                                                        raise TypeError(msg)
-
-                                                                                                                                                        if div == 0:
-                                                                                                                                                                    msg = "division by zero"
-                                                                                                                                                                            raise ZeroDivisionError(msg)
-
-                                                                                                                                                                            new_m = [[round(j / div, 2) for j in i] for i in matrix]
-                                                                                                                                                                                return (new_m)
+    print("My name is {} {}".format(first_name, last_name))
