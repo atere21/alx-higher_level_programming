e93da0d93177cac1835d8940af3a0b70a78deba2
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-01-05 19:50:26 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,39 +1,128 @@
-#!/usr/bin/python3
-"""
-Function that prints a text with 2 new lines after each of these characters:
-    . ? and :
-    """
+The ``5-text_indentation`` module
+============================
 
+Using ``text_indentation``
+---------------------
 
-    def text_indentation(text):
-            """
-                Prints a text replace some characters with 2 newlines
-                    Arguments:
-                        text: must be a string
-                            If not a string a TypeError is raised
-                                There is no space at the beginning at the end of each printed line
-                                    """
+Importing function from the module:
 
-                                        if not isinstance(text, str):
-                                                    msg = "text must be a string"
-                                                            raise TypeError(msg)
+    >>> text_indentation = __import__('5-text_indentation').text_indentation
 
-                                                            if len(text) == 0:
-                                                                        print(end='')
-                                                                                return None
+Printing a large message
 
-                                                                                textlist = []
-                                                                                    mark = 0
+    >>> text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio beatiorem! Iam ruinas videres""")
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    <BLANKLINE>
+    Quonam modo?
+    <BLANKLINE>
+    Utrum igitur tibi litteram videor an totas paginas commovere?
+    <BLANKLINE>
+    Non autem hoc:
+    <BLANKLINE>
+    igitur ne illud quidem.
+    <BLANKLINE>
+    Fortasse id optimum, sed ubi illud:
+    <BLANKLINE>
+    Plus semper voluptatis?
+    <BLANKLINE>
+    Teneo, inquit, finem illi videri nihil dolere.
+    <BLANKLINE>
+    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum rationi oboediens.
+    <BLANKLINE>
+    Si id dicis, vicimus.
+    <BLANKLINE>
+    Inde sermone vario sex illa a Dipylo stadia confecimus.
+    <BLANKLINE>
+    Sin aliud quid voles, postea.
+    <BLANKLINE>
+    Quae animi affectio suum cuique tribuens atque hanc, quam dico.
+    <BLANKLINE>
+    Utinam quidem dicerent alium alio beatiorem! Iam ruinas videres
 
-                                                                                        for i in range(len(text)):
-                                                                                                    if text[i] == '.' or text[i] == '?' or text[i] == ':':
-                                                                                                                    textlist.append(text[mark: i + 1] + "\n\n")
-                                                                                                                                mark = i + 1
+Printing a word with spaces around
 
-                                                                                                                                    textlist.append(text[mark:i + 1])
+    >>> text_indentation("    Holberton    ")
+    Holberton
 
-                                                                                                                                        textlist = [i.strip(' ') for i in textlist]
+Printing a word with a special character at the end
 
-                                                                                                                                            new_text = "".join(textlist)
+    >>> text_indentation("    Hello    .")
+    Hello.
+    <BLANKLINE>
 
-                                                                                                                                                print(new_text, end='')
+Printing all special characters
+
+    >>> text_indentation(".?:")
+    .
+    <BLANKLINE>
+    ?
+    <BLANKLINE>
+    :
+    <BLANKLINE>
+
+Passing Node as a text
+
+    >>> text_indentation(None)
+    Traceback (most recent call last):
+    	      ...
+    TypeError: text must be a string
+
+Passing a number as a text
+
+    >>> text_indentation(10)
+    Traceback (most recent call last):
+    	      ...
+    TypeError: text must be a string
+
+Printing a text with special characters
+
+    >>> text_indentation("Betty: Holberton? Python is. cool   ")
+    Betty:
+    <BLANKLINE>
+    Holberton?
+    <BLANKLINE>
+    Python is.
+    <BLANKLINE>
+    cool
+
+Printing a char number
+
+    >>> text_indentation('2')
+    2
+
+Passing an empty string
+
+    >>> text_indentation('    ')
+
+Passing a new line as a text
+
+    >>> text_indentation('\n')
+    <BLANKLINE>
+
+Passing a special character and some special characters
+
+    >>> text_indentation('\n.\n?\n:')
+    <BLANKLINE>
+    .
+    <BLANKLINE>
+    <BLANKLINE>
+    ?
+    <BLANKLINE>
+    <BLANKLINE>
+    :
+    <BLANKLINE>
+
+Passing a text with spaces and one new line
+
+    >>> text_indentation("   \n")
+    <BLANKLINE>
+
+Passing a text with spaces and one new line 2
+
+   >>> text_indentation("   \n   ")
+   <BLANKLINE>
+
+Passing a text with spaces and one new line 3
+
+   >>> text_indentation("\n   ")
+   <BLANKLINE>
