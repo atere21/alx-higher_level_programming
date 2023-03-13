081022357621481679eb953b782ecf8414d1651b
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-13 20:35:36 +0100

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 // Script that prints "JavaScript is amazing".
-//
-// const myVar = 'JavaScript is amazing';
-// console.log(myVar);
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
