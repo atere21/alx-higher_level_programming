a5e291abc8e3f10f71cbd36e8234c22f6b2a7c9c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-14 20:52:31 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,9 +2,9 @@
 const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-	  if (newDict[dict[key]] === undefined) {
-		      newDict[dict[key]] = [];
-		    }
-	  newDict[dict[key]].push(key);
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
 }
 console.log(newDict);
