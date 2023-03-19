a065efb6bd116c9d27e59561d4a6f840d9e99fad
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-19 23:45:02 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """
 This script  takes in the name of a state
 as an argument and lists all cities of that
@@ -9,33 +9,33 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-        """
-            Access to the database and get the cities
-                from the database.
-                    """
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-                            with db.cursor() as cur:
-                                        cur.execute("""
-                                                    SELECT
-                                                                    cities.id, cities.name
-                                                                                FROM
-                                                                                                cities
-                                                                                                            JOIN
-                                                                                                                            states
-                                                                                                                                        ON
-                                                                                                                                                        cities.state_id = states.id
-                                                                                                                                                                    WHERE
-                                                                                                                                                                                    states.name LIKE BINARY %(state_name)s
-                                                                                                                                                                                                ORDER BY
-                                                                                                                                                                                                                cities.id ASC
-                                                                                                                                                                                                                        """, {
-                                                                                                                                                                                                                                        'state_name': argv[4]
-                                                                                                                                                                                                                                                })
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
 
-                                                                                                                                                                                                                                rows = cur.fetchall()
+        rows = cur.fetchall()
 
-                                                                                                                                                                                                                                    if rows is not None:
-                                                                                                                                                                                                                                                print(", ".join([row[1] for row in rows]))
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
