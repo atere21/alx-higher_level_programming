5f1f5b5a3b6a60cbeefc9022a36ff977526dfa28
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-20 00:08:44 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,17 +10,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-        """
-            Access to the database and get the states
-                from the database.
-                    """
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-                        db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                                        argv[1], argv[2], argv[3])
-                            engine = create_engine(db_uri)
-                                Session = sessionmaker(bind=engine)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
 
-                                    session = Session()
+    session = Session()
 
-                                        for instance in session.query(State).order_by(State.id):
-                                                    print('{0}: {1}'.format(instance.id, instance.name))
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
