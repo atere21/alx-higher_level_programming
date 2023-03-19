37364d4d4d805e03b8d3f1a84c4bbb9e7a62a4a1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-19 23:33:18 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,6 @@
-s script takes in an argument and
+#!/usr/bin/python3
+"""
+This script takes in an argument and
 displays all values in the states
 where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
@@ -8,19 +10,19 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-        """
-            Access to the database and get the states
-                from the database.
-                    """
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                                 passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-                                                     cur = db.cursor()
-                                                         cur.execute("SELECT * FROM states \
-                                                                                  WHERE name LIKE BINARY '{}' \
-                                                                                                   ORDER BY states.id ASC".format(argv[4]))
-                                                                                                       rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
 
-                                                                                                           for row in rows:
-                                                                                                                   print(row)
+    for row in rows:
+        print(row)
