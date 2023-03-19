354515db5915ceb8ad05448aa3564c79322def51
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-19 23:36:57 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,30 +12,30 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-        """
-            Access to the database and get the states
-                from the database.
-                    """
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                                 passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-                                                     with db.cursor() as cur:
-                                                             cur.execute("""
-                                                                         SELECT
-                                                                                         *
-                                                                                                     FROM
-                                                                                                                     states
-                                                                                                                                 WHERE
-                                                                                                                                                 name LIKE BINARY %(name)s
-                                                                                                                                                             ORDER BY
-                                                                                                                                                                             states.id ASC
-                                                                                                                                                                                     """, {
-                                                                                                                                                                                                 'name': argv[4]
-                                                                                                                                                                                                         })
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
 
-                                                                                                                                                                                                                 rows = cur.fetchall()
+        rows = cur.fetchall()
 
-                                                                                                                                                                                                                     if rows is not None:
-                                                                                                                                                                                                                             for row in rows:
-                                                                                                                                                                                                                                         print(row)
+    if rows is not None:
+        for row in rows:
+            print(row)
