154f32dbc9217acd3f5e33cf6c3152d702f61b0a
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-19 23:39:09 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,30 +8,30 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-        """
-            Access to the database and get the cities
-                from the database.
-                    """
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-                            with db.cursor() as cur:
-                                        cur.execute("""
-                                                    SELECT
-                                                                    cities.id, cities.name, states.name
-                                                                                FROM
-                                                                                                cities
-                                                                                                            JOIN
-                                                                                                                            states
-                                                                                                                                        ON
-                                                                                                                                                        cities.state_id = states.id
-                                                                                                                                                                    ORDER BY
-                                                                                                                                                                                    cities.id ASC
-                                                                                                                                                                                            """)
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
 
-                                                                                                                                                                                                    rows = cur.fetchall()
+        rows = cur.fetchall()
 
-                                                                                                                                                                                                        if rows is not None:
-                                                                                                                                                                                                                for row in rows:
-                                                                                                                                                                                                                            print(row)
+    if rows is not None:
+        for row in rows:
+            print(row)
