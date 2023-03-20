5f2104c3ed020cec7042ede4296ed82ba9322b2c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-20 03:03:04 +0100

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,18 +10,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-        """
-            Updates a State object on the database.
-                """
+    """
+    Updates a State object on the database.
+    """
 
-                    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                                    argv[1], argv[2], argv[3])
-                        engine = create_engine(db_uri)
-                            Session = sessionmaker(bind=engine)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
 
-                                session = Session()
+    session = Session()
 
-                                    ari_state = session.query(State).filter(State.id == '2').first()
-                                        ari_state.name = 'New Mexico'
-                                            session.commit()
-                                                session.close()
+    ari_state = session.query(State).filter(State.id == '2').first()
+    ari_state.name = 'New Mexico'
+    session.commit()
+    session.close()
