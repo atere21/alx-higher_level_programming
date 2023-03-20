a176ec214eac3ded75d3ca0418d06241dd9c7490
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-20 03:08:16 +0100

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,18 +10,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                                                      pool_pre_ping=True)
-            Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-                Session = sessionmaker(bind=engine)
-                    session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                        st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-                            for state in st:
-                                        print("{}: {}".format(state.id, state.name))
-                                                for city in state.cities:
-                                                                print("    {}: {}".format(city.id, city.name))
-
+    for state in st:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
