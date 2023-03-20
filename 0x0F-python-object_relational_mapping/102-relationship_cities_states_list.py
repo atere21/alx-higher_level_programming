93f96b2a66ac4cb2e0e14ee1b3a39e9ac4c3f8f8
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-03-20 03:13:36 +0100

    Fixed

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -8,7 +8,6 @@ from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
